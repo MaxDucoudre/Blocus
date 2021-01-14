77d4922cded3d3835baa97eda69b6ad04fb4a5d5
Max Ducoudré <maxouducoudre@gmail.com>
2021-01-14 04:33:33 +0100

    Transférer les fichiers vers 'BLOCUS'

--- BLOCUS/verification.h
+++ BLOCUS/verification.h
@@ -4,5 +4,4 @@
 
 int verification_victoire(int taille_grille, int case_prise[taille_grille][taille_grille], int coord_largeur_couleur, int coord_hauteur_couleur, int num_sprite_orange, int num_sprite_bleu);
 
-
 #endif
