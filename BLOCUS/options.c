0674217577f65c4fac4d88e88f87b2d5ad6fe31a
Max Ducoudré <maxouducoudre@gmail.com>
2021-01-15 03:41:29 +0100

    Transférer les fichiers vers 'BLOCUS'

--- BLOCUS/options.c
+++ BLOCUS/options.c
@@ -17,7 +17,7 @@ struct parametres menu_options(int i, struct parametres o) {
 	int hauteur_fenetre = 600;
 	int largeur_fenetre = 800;
 	short int options = 1;
-
+	int changement;
 	int enable_IA;
 	int taille_grille;
 	int hard_mod;
@@ -28,10 +28,7 @@ struct parametres menu_options(int i, struct parametres o) {
 	couleur lite_white = 15592941;
 
 
-
-
 	while(options == 1) { 
-
 
 		if(i==0) {
 			enable_IA = 1;
@@ -44,114 +41,141 @@ struct parametres menu_options(int i, struct parametres o) {
 		}
 
 
-		ChargerImageFond("image/muraille.jpg");
 
-		ChoisirCouleurDessin(orange);
-		EcrireTexte(largeur_fenetre*0.45, hauteur_fenetre*0.10, "Blocus !", 2);
+		ChargerImageFond("image/background/menu_option.png");
 
-		ChoisirCouleurDessin(dark_grey);
-		EcrireTexte(largeur_fenetre*0.05, hauteur_fenetre*0.20, "Options :", 2);
+		intelligence_artificielle();
+		dessiner_enable_ia(enable_IA);
+		difficulty(hard_mod);
+		dessiner_bouton_reset();
 
-
-		if(hard_mod == 1) {
-			bouton_options("Mode : Difficile", 5);
-		} else {
-			bouton_options("Mode : Normal", 5);
-		}
+		dessiner_bouton_enregistrer();
 
 
-		if(enable_IA == 1) {
-			bouton_options("Mode de jeux : 1 joueur", 3);
-		} else {
-			bouton_options("Mode de jeux : 2 joueurs", 3);
-		}
+		dessin_changement(2);
+		dessin_changement(1);
 
 
-		if(taille_grille == 3) {
-			bouton_options("Taille de la grille : 3x3", 4);
-		} else if (taille_grille == 4) {
-			bouton_options("Taille de la grille : 4x4", 4);
-		} else if (taille_grille == 5) {
-			bouton_options("Taille de la grille : 5x5", 4);
-		} else if (taille_grille == 6) {
-			bouton_options("Taille de la grille : 6x6", 4);
-		} else if (taille_grille == 7) {
-			bouton_options("Taille de la grille : 7x7", 4);
+		if(taille_grille == 9) {
+			dessin_taille_grille("9x9");
 		} else if (taille_grille == 8) {
-			bouton_options("Taille de la grille : 8x8", 4);
-		} else if (taille_grille == 9) {
-			bouton_options("Taille de la grille : 9x9", 4);
+			dessin_taille_grille("8x8");
+		} else if (taille_grille == 7) {
+			dessin_taille_grille("7x7");
+		} else if (taille_grille == 6) {
+			dessin_taille_grille("6x6");
+		} else if (taille_grille == 5) {
+			dessin_taille_grille("5x5");
+		} else if (taille_grille == 4) {
+			dessin_taille_grille("4x4");
+		} else if (taille_grille == 3) {
+			dessin_taille_grille("3x3");
 		}
+		
 
-		bouton_options("Reset", 8);
-		bouton_options("Enregistrer", 9);
 
 
 		do{
 			SourisPosition();
-			bouton_options_hover(3);
-			bouton_options_hover(5);
-			bouton_options_hover(4);
-			bouton_options_hover(8);
-			bouton_options_hover(9);
-
+			hover_enable_ia(enable_IA);
+			hover_difficulty(hard_mod);
+			hover_bouton_reset();
+			hover_bouton_enregistrer();
+			hover_changement(1);
+			hover_changement(2);
 
 			cliquee = SourisCliquee();
 			if(cliquee == 1) {
 
-				if(_X > largeur_fenetre*0.05 && _X < largeur_fenetre*0.05 + 350 && _Y > hauteur_fenetre*0.5 - 30 && _Y < hauteur_fenetre*0.50 + 10) {
+				if(_X > 320 && _X < 320+140 && _Y > 323 && _Y < 323+30) {
 					if(hard_mod == 1) {
-						bouton_options("Mode : Normal", 5);
 						hard_mod = 0;
+						difficulty(hard_mod);
+
 					} else {
-						bouton_options("Mode : Difficile", 5);
 						hard_mod = 1;
+						difficulty(hard_mod);
+
 					}
 				}
 
-				if(_X > largeur_fenetre*0.05 && _X < largeur_fenetre*0.05 + 350 && _Y > hauteur_fenetre*0.30 - 30 && _Y < hauteur_fenetre*0.30 + 10) {
+				if(_X > 125 && _X < 125+140 && _Y > 323 && _Y < 332+30) {
 					if(enable_IA == 1) {
-						bouton_options("Mode de jeux : 2 joueurs", 3);
 						enable_IA = 0;
+						dessiner_enable_ia(enable_IA);
+
 					} else {
-						bouton_options("Mode de jeux : 1 joueur", 3);
 						enable_IA = 1;
+						dessiner_enable_ia(enable_IA);
+
 					}
 				}
 
-				if(_X > largeur_fenetre*0.05 && _X < largeur_fenetre*0.05 + 350 && _Y > hauteur_fenetre*0.4 - 30 && _Y < hauteur_fenetre*0.4 + 10) {
+				if(_X > 260+60+20 && _X < 260+60+20+40 && _Y > 385 && _Y < 385+40) {
 					if(taille_grille == 3) {
-						bouton_options("Taille de la grille : 4x4", 4);
+						dessin_taille_grille("4x4");
 						taille_grille = 4;
 					} else if(taille_grille == 4) {
-						bouton_options("Taille de la grille : 5x5", 4);
+						dessin_taille_grille("5x5");
 						taille_grille = 5;
 					} else if(taille_grille == 5) {
-						bouton_options("Taille de la grille : 6x6", 4);
+						dessin_taille_grille("6x6");
 						taille_grille = 6;
 					} else if(taille_grille == 6) {
-						bouton_options("Taille de la grille : 7x7", 4);
+						dessin_taille_grille("7x7");
 						taille_grille = 7;
 					} else if(taille_grille == 7) {
-						bouton_options("Taille de la grille : 8x8", 4);
+						dessin_taille_grille("8x8");
 						taille_grille = 8;
 					} else if(taille_grille == 8) {
-						bouton_options("Taille de la grille : 9x9", 4);
+						dessin_taille_grille("9x9");
 						taille_grille = 9;
 					} else if(taille_grille == 9) {
-						bouton_options("Taille de la grille : 3x3", 4);
+						dessin_taille_grille("3x3");
 						taille_grille = 3;
 					}
 				}
 
-				if(_X > largeur_fenetre*0.05 && _X < largeur_fenetre*0.05 + 350 && _Y > hauteur_fenetre*0.8 - 30 && _Y < hauteur_fenetre*0.8 + 10) {
+				if(_X > 260-60 && _X < 260-60+40 && _Y > 385 && _Y < 385+40) {
+					if(taille_grille == 3) {
+						dessin_taille_grille("9x9");
+						taille_grille = 9;
+					} else if(taille_grille == 9) {
+						dessin_taille_grille("8x8");
+						taille_grille = 8;
+					} else if(taille_grille == 8) {
+						dessin_taille_grille("7x7");
+						taille_grille = 7;
+					} else if(taille_grille == 7) {
+						dessin_taille_grille("6x6");
+						taille_grille = 6;
+					} else if(taille_grille == 6) {
+						dessin_taille_grille("5x5");
+						taille_grille = 5;
+					} else if(taille_grille == 5) {
+						dessin_taille_grille("4x4");
+						taille_grille = 4;
+					} else if(taille_grille == 4) {
+						dessin_taille_grille("3x3");
+						taille_grille = 3;
+					}
+				}
+
+				printf("%d %d\n", _X, _Y);
+
+				if(_X > 490 && _X < 515 && _Y > 234 && _Y < 255) {
+					return o;
+				}
+
+
+				if(_X > 200 && _X < 200+60 && _Y > 475 && _Y < 490) {
 					i=0;
 					puts("Reset");
 					break;
 				}
 
 
-				if(_X > largeur_fenetre*0.05 && _X < largeur_fenetre*0.05 + 350 && _Y > hauteur_fenetre*0.9 - 30 && _Y < hauteur_fenetre*0.9 + 10) {
+				if(_X > 340 && _X < 385 && _Y > 475 && _Y < 490) {
 					options = 0;
 					puts("Enregistrer");
 					o.grille = taille_grille;
@@ -165,7 +189,9 @@ struct parametres menu_options(int i, struct parametres o) {
 
 
 		} while (options == 1);
+		
 	}
+
 
 
 	
